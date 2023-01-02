945c53829f8fdeeae6147fb52823d42f30baf762
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-02 12:42:20 -0500

    feat: cria o component pokeCard

---
+++ src/components/pokeCard/pokeCard.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+// import { Container } from './styles';
+
+function PokeCard({ url }) {
+  const [pokeInfo, setPokeInfo] = useState(null);
+
+  const pokemon = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPokeInfo(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    pokemon();
+  }, []);
+
+  console.log(pokeInfo?.sprites.other['official-artwork'].front_default);
+  return (
+    <div>
+      <h3>{pokeInfo?.name}</h3>
+      <img
+        src={pokeInfo?.sprites.other['official-artwork'].front_default}
+        alt={pokeInfo?.name}
+      />
+      <p>{`Level - ${pokeInfo?.base_experience}`}</p>
+    </div>
+  );
+}
+
+export default PokeCard;
