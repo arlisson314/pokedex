b60253cd88843fc9178034d657a437542986a67a
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-03 22:30:58 -0500

    style: adiciona estilos

---
+++ src/components/input/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  >input {
+      padding-left: 1ch;
+      margin-top: -1.6em;
+      outline: none;
+      height: 2em;
+      border-radius: 5px;
+      border: none;
+    }
+`;
+export default Container;
