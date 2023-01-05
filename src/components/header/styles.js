26c467d236bdc597b5b0b16268471e23ed9f538b
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-04 22:55:14 -0500

    style: adiciona estilos

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 1em;
+  width: 100%;
   >img {
     width: 15em;
     height:5.2em;
