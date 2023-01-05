6d9fd87d2f48d6627a7c66cffb8493ee33550451
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-04 22:56:05 -0500

    feat: cria component e arquivo de estilos Favorites

---
+++ src/pages/favorites/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import Container from './styles';
+
+// import Container from './styles';
+
+function Favorites() {
+  return (
+    <Container>
+      <Header />
+      <section>
+        <div>card</div>
+        <div>sobre</div>
+      </section>
+    </Container>
+  );
+}
+
+export default Favorites;
