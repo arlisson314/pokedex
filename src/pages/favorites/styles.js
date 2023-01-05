e1d62ad5e2ce53d10e74dcabae448a49e0520dcf
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-05 14:27:50 -0500

    style: adicona estilo ao componente favorites

--- src/pages/favorites/styles.js
+++ src/pages/favorites/styles.js
@@ -1,6 +1,31 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  justify-content: center;
+  flex-direction: column;
+  border: solid 1px red;
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  
+  >section {
+    border: solid 1px black;
+    display: flex;
+    height: 100%;
+    width: 90%;
+  }
+  >section div:nth-child(1) {
+    border: solid 1px yellow;
+    height: 6em;
+    width: 5em;
+  }
+  >section div:nth-child(2) {
+    border: solid 1px yellow;
+    height: 6em;
+    width: 5em;
+  }
+  
 `;
 
 export default Container;
