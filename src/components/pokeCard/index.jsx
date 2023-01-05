405a723e73308bb16c97849129cd6fec51169065
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-05 14:26:35 -0500

    feat: adiciona id as cards

--- src/components/pokeCard/index.jsx
+++ src/components/pokeCard/index.jsx
@@ -21,7 +21,7 @@ function PokeCard({ url }) {
         src={pokeInfo?.sprites.other['official-artwork'].front_default}
         alt={pokeInfo?.name}
       />
-      <h3>{pokeInfo?.name}</h3>
+      <h3>{`${pokeInfo?.id}. ${pokeInfo?.name}`}</h3>
       <p>{`Level - ${pokeInfo?.base_experience}`}</p>
       {pokeInfo?.types.length > 1
         ? (<p>{`Tipos - ${pokeInfo?.types[0].type.name}, ${pokeInfo?.types[1].type.name}`}</p>)
