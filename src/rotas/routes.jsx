84cc22c4e0bb42b3c5a683624b2dff99aad1b3f2
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-04 22:57:16 -0500

    feat: adiciona rota favorites

--- src/rotas/routes.jsx
+++ src/rotas/routes.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Favorites from '../pages/favorites';
 import Home from '../pages/home';
+import Login from '../pages/login';
 
 function MainRouts() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} /> */}
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
 }
