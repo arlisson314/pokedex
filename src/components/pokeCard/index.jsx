91cf1e577267acbc7b6895bd12007168d54b3980
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-05 19:31:06 -0500

    feat: adiciona butão de favoritar card

--- src/components/pokeCard/index.jsx
+++ src/components/pokeCard/index.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { AiTwotoneStar } from 'react-icons/ai';
 import Container from './styles';
-
+// AiOutlineStar
+// AiFillStar
+// AiTwotoneStar
 function PokeCard({ url }) {
   const [pokeInfo, setPokeInfo] = useState(null);
-
   const getokemon = () => {
     axios.get(url)
       .then((result) => setPokeInfo(result.data))
@@ -15,6 +18,14 @@ function PokeCard({ url }) {
     getokemon();
   }, []);
 
+  const test = () => console.log('oi');
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <Container>
       <img
@@ -22,7 +33,10 @@ function PokeCard({ url }) {
         alt={pokeInfo?.name}
       />
       <h3>{`${pokeInfo?.id}. ${pokeInfo?.name}`}</h3>
-      <p>{`Level - ${pokeInfo?.base_experience}`}</p>
+      <p>
+        {`Level - ${pokeInfo?.base_experience}`}
+        <button type="button" onClick={test} onKeyDown={(e) => handleKeyDown(e)}><AiTwotoneStar /></button>
+      </p>
       {pokeInfo?.types.length > 1
         ? (<p>{`Tipos: ${pokeInfo?.types[0].type.name}, ${pokeInfo?.types[1].type.name}`}</p>)
         : (<p>{`Tipo: ${pokeInfo?.types[0].type.name}`}</p>
