800a05eb715b45793dc74463a300695b21a9784c
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-05 20:07:08 -0500

    style: remove estilo

--- src/components/pokeCard/styles.js
+++ src/components/pokeCard/styles.js
@@ -7,46 +7,10 @@ const Container = styled.div`
   justify-content: space-between;
   background-color: #dbdbdb;
   border: 1px black solid;
-  flex-direction: column;
-  align-items: center;
   border-radius: 5px;
   margin-top: 0.5em;
-  display : flex;
   height: 13.5em;
   width: 12em;
-    >img {
-      border-bottom: 3px solid black;
-      background: red;
-      height: 8em;
-      width: 100%;
-    }
-
-    >h3 {
-      font-size: 1.2em;
-    }
-    >p{
-      font-weight: bold;
-      font-size: 1em;
-      padding: 1px;
-      display: flex;
-      align-items: center;
-      gap: 1ch;
-    
-      >button {
-        font-size: 1.5em;
-        display: flex;
-        align-self: center;
-        color: yellow;
-        border: none;
-        background: none;
-      }
-    }
-    
-    >p:nth-of-type(2) {
-      margin-bottom: 0.5em;
-      font-size: 1em;
-      font-weight: bold;
-    }
 `;
 
 export default Container;
