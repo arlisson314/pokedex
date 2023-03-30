797b7eeed2eb38a6a3e14e23642f09fd748671b4
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-30 09:29:44 -0300

    refactor: Remove rota /home

--- src/rotas/routes.jsx
+++ src/rotas/routes.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Favorites from '../pages/favorites';
 import Home from '../pages/home';
-import Login from '../pages/login';
 
 function MainRouts() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
